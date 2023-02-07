34f77bd5b723b47c579999b03dff24977da165e2
ShubhangiGhorpade <122660085+ShubhangiGhorpade@users.noreply.github.com>
2023-02-07 23:06:51 +0530

    Add files via upload

---
+++ Association/src/com/java/association/main/Brand.java
@@ -0,0 +1,16 @@
+package com.java.association.main;
+
+public class Brand {
+	public String type;
+	public String colour;
+
+	public void init(String type, String colour) {
+		this.type=type;
+		this.colour=colour;
+	}
+	public void show() {
+		System.out.println("Type :"+this.type);
+		System.out.println("Colour :"+this.colour);
+	}
+
+}
